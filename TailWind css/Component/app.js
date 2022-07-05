cea932472e04b71da8964f8617cbf2fd88fef09a
theChildOfProphecy218 <daveabhi218@gmail.com>
2022-07-05 17:44:37 +0530

    done

---
+++ TailWind css/Component/app.js
@@ -0,0 +1,78 @@
+const dollarFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+});
+
+document.addEventListener('alpine:init',() => {
+    Alpine.store('menu',{
+        items: [],
+        cart: [],
+
+        init() {
+            this.items = menuItems;
+        },
+
+        displayDollar(value) {
+            return dollarFormatter.format(value);
+        },
+    })
+
+    //Data
+    Alpine.data('menu', () => ({
+        addToCart(itemId) {
+          const item = this.$store.menu.items[itemId];
+          this.$store.menu.cart.push({ ...item, count: 1 });
+        },
+    
+        itemInCart(itemId) {
+          const itemName = this.$store.menu.items[itemId].name;
+          return this.$store.menu.cart.some((item) => item.name === itemName);
+        },
+      })
+    );
+
+    Alpine.data('cart', () => ({
+        cartPrice: { subTotal: 0, tax: 0, total: 0 },
+        get cartEmpty() {
+          return this.$store.menu.cart.length === 0;
+        },
+    
+        init() {
+          this.$watch('$store.menu.cart', () => {
+            this.cartTotal();
+          });
+        },
+    
+        increaseItem(itemId) {
+          this.$store.menu.cart[itemId].count++;
+        },
+        decreaseItem(itemId) {
+          // If 1 item in cart, remove from cart
+          if (this.$store.menu.cart[itemId].count === 1) {
+            this.$store.menu.cart.splice(itemId, 1);
+            return;
+          }
+    
+          this.$store.menu.cart[itemId].count--;
+        },
+    
+        itemTotal(item) {
+          const itemTotal = item.price * item.count;
+          return dollarFormatter.format(itemTotal);
+        },
+    
+        cartTotal() {
+          const restaurantTax = 0.05; // 5.0% avg VA
+    
+          const subTotal = this.$store.menu.cart.reduce(
+            (total, item) => total + item.price * item.count,
+            0
+          );
+          const tax = subTotal * restaurantTax;
+          const total = subTotal + tax;
+    
+          this.cartPrice = { subTotal, tax, total };
+        },
+      }));    
+})
---
+++ TailWind css/Component/data-menu.js
@@ -0,0 +1,44 @@
+const menuItems = [
+    {
+      name: 'French Fries with Ketchup',
+      price: 2.23,
+      image: 'plate__french-fries.png',
+      alt: 'French Fries',
+      count: 0,
+    },
+    {
+      name: 'Burger',
+      price: 5.12,
+      image: 'burger.png',
+      alt: 'Burger',
+      count: 0,
+    },
+    {
+      name: 'Noodles',
+      price: 7.82,
+      image: 'noodles.png',
+      alt: 'Noodles',
+      count: 0,
+    },
+    {
+      name: 'Pasta',
+      price: 5.99,
+      image: 'pasta.png',
+      alt: 'Pasta',
+      count: 0,
+    },
+    {
+      name: 'Pizza',
+      price: 6.98,
+      image: 'pizza.png',
+      alt: 'Pizza',
+      count: 0,
+    },
+    {
+      name: 'Soup',
+      price: 6.34,
+      image: 'soup.png',
+      alt: 'Soup',
+      count: 0,
+    },
+  ];
